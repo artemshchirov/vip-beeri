8af9c7891a093839cb0c0a747c692c8c4df1d337
Artem Shchirov <artemshchirov@gmail.com>
2023-02-06 12:58:22 -0800

    Initial commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import Page from '../layouts/Page';
+
+const About: FC = () => {
+  return (
+    <Page>
+      
+    </Page>
+  );
+};
+
+export default About;
---
+++ src/utils/constants.ts
@@ -0,0 +1,25 @@
+import photo_0 from '../assets/rabbit.png';
+import photo_1 from '../assets/cat.png';
+import photo_2 from '../assets/bear.png';
+
+export const team = [
+  {
+    name: 'Artem',
+    role: 'FullStack Developer, freelancer',
+    image: photo_1,
+    email: 'artemshchirov@gmail.com',
+    linkedin: 'https://www.linkedin.com/in/artemshchirov/',
+    github: 'https://github.com/artemshchirov',
+    link: 'https://artemshchirov.github.io/portfolio/',
+    id: 1,
+  },
+  {
+    name: 'Rabbit',
+    role: 'freelancer',
+    image: photo_0,
+    email: 'artemshchirov@gmail.com',
+    github: 'https://github.com/artemshchirov',
+    id: 0,
+  },
+
+];
