0d5f458c9357d62ce5fff4b1a56d503ba3b2327e
Artem Shchirov <artemshchirov@gmail.com>
2023-02-12 16:15:30 +0200

    feat(ui): update styles, buttons position

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,89 +25,100 @@ interface Row {
 const defaultValues: Row[] = [
   {
     id: 10,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 11,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 12,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 13,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 14,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 15,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 16,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 17,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 18,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 19,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 20,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 21,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
   {
     id: 22,
-    name: "ТЕСТ ТЕСТ ТЕСТ",
+    name: "Имя в таблице",
     date: "19-01-2023, чт",
     time: "00:25",
   },
 ];
 
 const dropdownValues: { id: number; name: string }[] = [
-  { id: 0, name: "Роман" },
+  { id: 0, name: "Роман Балабанов" },
   { id: 1, name: "Артём" },
-  { id: 2, name: "НОВОЕ ОЧЕНЬ ДЛИННОЕ БОЛЬШОЕ ИМЯ" },
-  { id: 3, name: "" },
+  { id: 2, name: "НОВОЕ ОЧЕНЬ ДЛИННОЕ БОЛЬШОЕ СЛОВО" },
+  { id: 14, name: "" },
+  { id: 3, name: "Имя" },
+  { id: 4, name: "Имя" },
+  { id: 5, name: "Имя" },
+  { id: 6, name: "Имя" },
+  { id: 7, name: "Имя" },
+  { id: 8, name: "Имя" },
+  { id: 9, name: "Имя" },
+  { id: 10, name: "Имя" },
+  { id: 11, name: "Имя" },
+  { id: 12, name: "Имя" },
+  { id: 13, name: "Имя" },
 ];
 
 interface ToastOptions {
@@ -179,7 +190,7 @@ const Home: React.FC = () => {
       showToast({
         severity: "success",
         summary: "Таблица обновлена",
-        detail: `Добавлено Имя: ${data.name}, Дата: ${data.date}`,
+        detail: `${data.name} — ${data.date}`,
       });
 
       formik.setFieldValue("date", formik.initialValues.date);
@@ -208,8 +219,8 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className="py-4 md:py-4 px-8 md:px-12 xl:px-20 lg:px-16 flex flex-col min-h-screen justify-between bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-indigo-200 via-slate-600 to-indigo-200">
-      <header className="px-4 md:px-4 xl:px-8 lg:px-6 py-2.5 bg-white border-gray-200 rounded-md dark:bg-gray-800">
+    <div className="flex flex-col min-h-screen justify-between bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-indigo-200 via-slate-600 to-indigo-200">
+      <header className="px-2 md:px-4 xl:px-8 lg:px-6 py-2.5 bg-white border-gray-200 dark:bg-gray-800">
         <nav className="container flex flex-wrap items-center justify-between mx-auto">
           <a className="flex items-center" href="#">
             <img src={logo} className="h-6 mr-3 sm:h-9" alt="App Logo" />
@@ -220,11 +231,11 @@ const Home: React.FC = () => {
         </nav>
       </header>
 
-      <main className="flex justify-center w-full p-4 no-scrollbar md:p-4">
+      <main className="flex justify-center w-full p-2 no-scrollbar md:p-4">
         <section className="container flex flex-wrap items-center justify-center ">
           <div className="flex justify-center w-full">
             <Calendar
-              className="w-full overflow-hidden text-xs rounded-md sm:w-full lg:w-6/12"
+              className="w-full text-xs sm:w-full lg:w-6/12"
               value={selectedDate}
               onChange={(e: CalendarChangeParams) => onCalendarChange(e)}
               inline
@@ -232,36 +243,38 @@ const Home: React.FC = () => {
           </div>
           <form
             onSubmit={formik.handleSubmit}
-            className="flex items-center w-full gap-2 my-4 sm:w-full lg:w-6/12"
+            className="flex flex-col md:flex-row items-center w-full gap-2 my-2 sm:w-full lg:w-6/12"
           >
-            <Toast ref={toast} />
-            <div className="w-full overflow-hidden rounded-md lg:w-4/12">
-              <Dropdown
-                inputId="name"
-                value={selectedName}
-                options={dropdownValues}
-                optionValue="id"
-                optionLabel="name"
-                placeholder={selectedName || "Имя"}
-                className={classNames("w-full", {
-                  "p-invalid": isFormFieldInvalid("name") && !selectedName,
-                })}
-                onChange={(e: DropdownChangeParams) => onDropdownChange(e)}
-              />
+            <div className="flex flex-row w-full gap-2 md:w-8/12">
+              <Toast ref={toast} className="pl-5" />
+              <div className="w-full ">
+                <Dropdown
+                  inputId="name"
+                  value={selectedName}
+                  options={dropdownValues}
+                  optionValue="id"
+                  optionLabel="name"
+                  placeholder={selectedName || "Имя"}
+                  className={classNames("w-full", {
+                    "p-invalid": isFormFieldInvalid("name") && !selectedName,
+                  })}
+                  onChange={(e: DropdownChangeParams) => onDropdownChange(e)}
+                />
+              </div>
+              <div className="w-full ">
+                <InputText
+                  name="date"
+                  value={selectedDate}
+                  className={classNames("w-full", {
+                    "p-invalid": isFormFieldInvalid("date") && !selectedDate,
+                  })}
+                  placeholder={selectedDate || "Дата"}
+                  disabled={selectedDate === formik.initialValues.date}
+                  readOnly
+                />
+              </div>
             </div>
-            <div className="w-full overflow-hidden rounded-md lg:w-4/12">
-              <InputText
-                name="date"
-                value={selectedDate}
-                className={classNames("w-full", {
-                  "p-invalid": isFormFieldInvalid("date") && !selectedDate,
-                })}
-                placeholder={selectedDate || "Дата"}
-                disabled={selectedDate === formik.initialValues.date}
-                readOnly
-              />
-            </div>
-            <div className="w-full overflow-hidden rounded-md lg:w-4/12">
+            <div className="w-full md:w-4/12">
               <Button
                 className="w-full whitespace-nowrap"
                 type="submit"
@@ -275,7 +288,7 @@ const Home: React.FC = () => {
           </form>
           <div className="flex justify-center w-full ">
             <DataTable
-              className="w-full overflow-hidden text-xs rounded-md sm:w-full lg:w-6/12"
+              className="w-full text-xs sm:w-full lg:w-6/12"
               value={tableRows}
               selectionMode="single"
               selection={selectedRow}
@@ -295,7 +308,7 @@ const Home: React.FC = () => {
         </section>
       </main>
 
-      <footer className="px-4 md:px-4 xl:px-8 lg:px-6 py-2.5 bg-white border-gray-200 rounded-md dark:bg-gray-800">
+      <footer className="px-4 md:px-4 xl:px-8 lg:px-6 py-2.5 bg-white border-gray-200  dark:bg-gray-800">
         <span className="container flex flex-wrap items-center h-6 mx-auto text-sm text-gray-500 sm:h-9 sm:text-center dark:text-gray-400">
           © 2023&nbsp;
           <a href="#" className="hover:underline">
