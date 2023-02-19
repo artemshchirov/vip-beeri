07425b29d2a3bc84e500bcef193d107f035eea50
Artem Shchirov <artemshchirov@gmail.com>
2023-02-20 01:48:36 +0200

    fex(deploy): fix refactor firebaseConfig

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,23 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID
+// };
+
+// FIXME move to .env
 const firebaseConfig = {
-  apiKey: 'AIzaSyBXixAp03PgzmRbq3yNzC2D3a_wdLOZ6VM',  // TODO move to .env
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  apiKey: 'AIzaSyBXixAp03PgzmRbq3yNzC2D3a_wdLOZ6VM',
+  authDomain: 'authDomain',
+  projectId: 'work-calendar-vip',
+  storageBucket: 'work-calendar-vip.appspot.com',
+  messagingSenderId: '808021288157',
+  appId: '1:808021288157:web:8823df99a48f253c8c2734'
 };
 
 export const app = initializeApp(firebaseConfig);
