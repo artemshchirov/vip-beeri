9daa1c0210fb721a3eb4067cdcf69381a695fefd
Artem Shchirov <artemshchirov@gmail.com>
2023-05-24 21:40:10 +0300

    fix(ui): del worker

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -14,12 +14,11 @@ const workers: Worker[] = [
   { id: 6, name: 'Avihai Yona' },
   { id: 7, name: 'Yaniv Mezgauker' },
   { id: 8, name: 'Moti' },
-  { id: 9, name: 'Djo Megan' },
-  { id: 10, name: 'Mihael Kadimov' },
-  { id: 11, name: 'Noa Avram' },
-  { id: 12, name: 'Liel Batito' },
-  { id: 13, name: 'Zead' },
-  { id: 14, name: 'Roman Balabanov', admin: true },
+  { id: 9, name: 'Mihael Kadimov' },
+  { id: 10, name: 'Noa Avram' },
+  { id: 11, name: 'Liel Batito' },
+  { id: 12, name: 'Zead' },
+  { id: 13, name: 'Roman Balabanov', admin: true },
 ];
 
 export const admins = workers.filter(({ admin }) => admin).map(({ name }) => name.toLowerCase());
